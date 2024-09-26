fcdbdbddb4d9696562b5107279e256d076b70c3f
Andrii Voznesenskyi <voznesenskijandrej5@gmail.com>
2024-09-27 01:57:24 +0200

    (#2) update scripts [pack-all-force]

---
+++ src/Nuar.Host/src/Nuar.Host/Program.cs
@@ -0,0 +1,29 @@
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+
+namespace Nuar.Host
+{
+    [ExcludeFromCodeCoverage]
+    public static class Program
+    {
+        public static Task Main(string[] args)
+            => CreateHostBuilder(args).Build().RunAsync();
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Microsoft.Extensions.Hosting.Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.ConfigureAppConfiguration(builder =>
+                        {
+                            var configPath = args?.FirstOrDefault() ?? "nuar.yml";  
+                            builder.AddYamlFile(configPath, false);
+                        })
+                        .ConfigureServices(services => services.AddNuar())  
+                        .Configure(app => app.UseNuar());
+                });
+    }
+}
