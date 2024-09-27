852abec02af273db6b16ab2045e46f0689fc6858
Andrii Voznesenskyi <voznesenskijandrej5@gmail.com>
2024-09-27 02:27:04 +0200

    (#8) add jwt configuration class lib

---
+++ src/Nuar.JWT/src/Nuar.JWT/JwtExtension.cs
@@ -0,0 +1,88 @@
+using System;
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+
+namespace Nuar.Extensions.Jwt
+{
+    internal class JwtExtension : IExtension
+    {
+        public string Name => "jwt";
+        public string Description => "JSON Web Token authentication";
+        public string Version => "1.0.0";
+
+        public void Add(IServiceCollection services, IOptionsProvider optionsProvider)
+        {
+            var options = optionsProvider.GetForExtension<JwtOptions>(Name);
+
+            services.AddAuthorization();
+
+            var tokenValidationParameters = new TokenValidationParameters
+            {
+                RequireAudience = options.RequireAudience,
+                ValidIssuer = options.ValidIssuer,
+                ValidIssuers = options.ValidIssuers,
+                ValidateActor = options.ValidateActor,
+                ValidAudience = options.ValidAudience,
+                ValidAudiences = options.ValidAudiences,
+                ValidateAudience = options.ValidateAudience,
+                ValidateIssuer = options.ValidateIssuer,
+                ValidateLifetime = options.ValidateLifetime,
+                ValidateTokenReplay = options.ValidateTokenReplay,
+                ValidateIssuerSigningKey = options.ValidateIssuerSigningKey,
+                RequireExpirationTime = options.RequireExpirationTime,
+                RequireSignedTokens = options.RequireSignedTokens,
+                ClockSkew = TimeSpan.Zero 
+            };
+
+            if (!string.IsNullOrWhiteSpace(options.AuthenticationType))
+            {
+                tokenValidationParameters.AuthenticationType = options.AuthenticationType;
+            }
+
+            if (!string.IsNullOrWhiteSpace(options.IssuerSigningKey))
+            {
+                tokenValidationParameters.IssuerSigningKey = new SymmetricSecurityKey(
+                    Encoding.UTF8.GetBytes(options.IssuerSigningKey));
+            }
+
+            if (!string.IsNullOrWhiteSpace(options.NameClaimType))
+            {
+                tokenValidationParameters.NameClaimType = options.NameClaimType;
+            }
+
+            if (!string.IsNullOrWhiteSpace(options.RoleClaimType))
+            {
+                tokenValidationParameters.RoleClaimType = options.RoleClaimType;
+            }
+
+            services.AddSingleton(tokenValidationParameters);
+
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(JwtBearerDefaults.AuthenticationScheme, o =>
+                {
+                    o.Authority = options.Authority;
+                    o.Audience = options.Audience;
+                    o.MetadataAddress = options.MetadataAddress;
+                    o.SaveToken = options.SaveToken;
+                    o.RefreshOnIssuerKeyNotFound = options.RefreshOnIssuerKeyNotFound;
+                    o.RequireHttpsMetadata = options.RequireHttpsMetadata;
+                    o.IncludeErrorDetails = options.IncludeErrorDetails;
+                    o.TokenValidationParameters = tokenValidationParameters;
+
+                    if (!string.IsNullOrWhiteSpace(options.Challenge))
+                    {
+                        o.Challenge = options.Challenge;
+                    }
+                });
+        }
+
+        public void Use(IApplicationBuilder app, IOptionsProvider optionsProvider)
+        {
+            app.UseAuthentication();
+            app.UseAuthorization();
+        }
+    }
+}
